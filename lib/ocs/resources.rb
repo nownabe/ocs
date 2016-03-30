b91b154968f4131ebafa948f0ab4266ba4e09542
nownabe <nownabe@gmail.com>
2016-03-30 09:52:03 +0900

    :art: Use require instead of autoload
    In multi threads, autoload can raise error.

--- lib/ocs/resources.rb
+++ lib/ocs/resources.rb
@@ -3,25 +3,7 @@ require "ocs/resources/base"
 
 module Ocs
   module Resources
-    autoload :Account,         "ocs/resources/account"
-    autoload :Address,         "ocs/resources/address"
-    autoload :AffinityGroup,   "ocs/resources/affinity_group"
-    autoload :AsyncJob,        "ocs/resources/async_job"
-    autoload :DiskOffering,    "ocs/resources/disk_offering"
-    autoload :Domain,          "ocs/resources/domain"
-    autoload :Group,           "ocs/resources/group"
-    autoload :Host,            "ocs/resources/host"
-    autoload :Iso,             "ocs/resources/iso"
-    autoload :Network,         "ocs/resources/network"
-    autoload :Nic,             "ocs/resources/nic"
-    autoload :OsType,          "ocs/resources/os_type"
-    autoload :ResourceDetail,  "ocs/resources/resource_detail"
-    autoload :SecurityGroup,   "ocs/resources/security_group"
-    autoload :ServiceOffering, "ocs/resources/service_offering"
-    autoload :SshKeyPair,      "ocs/resources/ssh_key_pair"
-    autoload :Tag,             "ocs/resources/tag"
-    autoload :Template,        "ocs/resources/template"
-    autoload :VirtualMachine,  "ocs/resources/virtual_machine"
-    autoload :Zone,            "ocs/resources/zone"
   end
 end
+
+Dir[File.expand_path("../resources/**/*.rb", __FILE__)].each { |f| require f }
